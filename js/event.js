4ee06cc5e08fc38ae42c581f9f5b4af2d089d8e9
kawinnat09 <kwinnathmyura@gmail.com>
2021-09-21 11:46:33 +0700

    update

---
+++ js/event.js
@@ -0,0 +1,12 @@
+
+function displayTime(){
+    document.getElementById("demo").innerHTML = Date();
+}
+
+function GoodMorning(){
+    document.getElementById("Morning").innerHTML = "GoodMorning";
+}
+
+function GoodAfternoon(){
+    document.write("Good Afternoon");
+}
